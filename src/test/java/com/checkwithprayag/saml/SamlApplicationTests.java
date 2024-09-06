648a4be6d5972ab9cb55e02f9ed2fdd668428974
prayag <prayagraj44@gmail.com>
2024-09-06 12:38:27 +0530

    Initial commit

---
+++ src/test/java/com/checkwithprayag/saml/SamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkwithprayag.saml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
