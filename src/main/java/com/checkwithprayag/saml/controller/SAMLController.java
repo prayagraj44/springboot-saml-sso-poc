71f86be44f90038759f79dbafa6b1779f1dd9575
prayag <prayagraj44@gmail.com>
2024-09-10 21:55:09 +0530

    cleanup

--- src/main/java/com/checkwithprayag/saml/controller/SAMLController.java
+++ src/main/java/com/checkwithprayag/saml/controller/SAMLController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SAMLController {
 
-    @GetMapping("/saml/SSO")
+    @GetMapping("/test-saml-sso")
     public String samlLogin() {
         return "SAML Login Successful!";
     }
